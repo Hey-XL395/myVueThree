606a1a00a7575137f5fba488756f0a7591c90dc0
Hey-XL395 <271305636@qq.com>
2021-04-16 20:51:59 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+*{margin:0;padding:0;}
+#app {
+  margin: 0;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/Loging/Loging.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="loging-page">
+    <div class="loging">This is an loging page</div>
+  </div>
+</template>
+<script lang="ts">
+// import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref, reactive } from "vue";
+
+export default defineComponent({
+  setup(props, { attrs, slots, emit }) {
+    console.log(props);
+    console.log(attrs);
+    console.log(slots);
+    console.log(emit);
+    return {};
+  },
+});
+</script>
+<style lang="scss">
+.loging-page {
+    width: 100%;
+    height: 100vh;
+    overflow-x: hidden;
+    background:repeating-linear-gradient(30deg, #79b, #79b 15%, #58a 0, #58a 30%);
+  .loging {
+    width: 60%;
+    margin: 0 auto;
+    margin-top: 10%;
+    height: 500px;
+    background: #fff;
+    border: 1px red solid;
+    border-radius: 5px;
+  }
+}
+</style>
+
